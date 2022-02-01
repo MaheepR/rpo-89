c5b8ed24e04b61744e771bf260c56581391dc1de
MaheepR <85050293+MaheepR@users.noreply.github.com>
2022-02-01 20:32:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA20_zZmEaL8Tx2q20bFwk5ZOeNWc19OwY",
+    authDomain: "storytellingapp-ede0d.firebaseapp.com",
+    databaseURL: "https://storytellingapp-ede0d-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-ede0d",
+    storageBucket: "storytellingapp-ede0d.appspot.com",
+    messagingSenderId: "201685445861",
+    appId: "1:201685445861:web:7edb3e56a8a6390b3bf80c"
+  };
